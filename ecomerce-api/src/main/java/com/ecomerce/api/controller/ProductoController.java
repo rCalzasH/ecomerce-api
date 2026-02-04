2ae7f10fe2ae81e495a0157b96055a9c0fec0511
rCalzasH <raulcalzashdez@gmail.com>
2026-02-04 18:43:15 +0100

    empiezo controller

--- ecomerce-api/src/main/java/com/ecomerce/api/controller/ProductoController.java
+++ ecomerce-api/src/main/java/com/ecomerce/api/controller/ProductoController.java
@@ -2,7 +2,44 @@ package com.ecomerce.api.controller;
 
 import com.ecomerce.api.model.Producto;
 import com.ecomerce.api.service.ProductoService;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/productos")
+
+public class ProductoController {
+    private final ProductoService p;
+    public Optional<Producto> encontrarPorId(Long id){
+        try {
+            return
+        } catch (Exception e) {
+            
+        }
+    }
+    
+	public Producto guardarProducto (Producto producto){
+
+    }
+
+    public Producto actualizarProductoId(Long id, Producto producto){
+
+    }
+
+	public Producto actualizarProductoPrecio(double precio, Producto producto){
+
+    }
+
+	public Producto actualizarProductoDescripcion(String descripcion, Producto producto){
+
+    }
+
+    public void borrarProductoPorId(Long id){
+
+    }
+}

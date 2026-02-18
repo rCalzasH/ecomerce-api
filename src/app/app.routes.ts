ee23fbc941ebcc3f896641bc7c2eb642c1f72e22
rCalzasH <raulcalzashdez@gmail.com>
2026-02-18 18:05:56 +0100

    forntend masivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Inicio } from './pages/inicio/inicio';
+import { LogIn } from './pages/log-in/log-in';
+import { Register } from './pages/register/register';
+import { Productos } from './pages/productos/productos';
+export const routes: Routes = [
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path: 'inicio', component: Inicio },
+    { path: 'logIn', component: LogIn},
+    { path: 'register', component: Register},
+    { path: 'Productos', component: Productos},
+];

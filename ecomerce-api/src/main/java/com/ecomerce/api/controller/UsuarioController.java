c2bc6bebd1ac810fd7557a4783e9bd86cf5799ea
PACHOOF <62396167+PACHOOF@users.noreply.github.com>
2026-03-05 16:20:26 +0100

    Creacion de la funcion login

--- ecomerce-api/src/main/java/com/ecomerce/api/controller/UsuarioController.java
+++ ecomerce-api/src/main/java/com/ecomerce/api/controller/UsuarioController.java
@@ -1,17 +1,22 @@
 package com.ecomerce.api.controller;
 
-import com.ecomerce.api.service.UsuarioService;
-import com.ecomerce.api.model.Rol;
-import com.ecomerce.api.model.Usuario;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecomerce.api.model.Rol;
+import com.ecomerce.api.model.Usuario;
+import com.ecomerce.api.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -21,13 +26,24 @@ public class UsuarioController{
     public ResponseEntity<Usuario> Resgister(@RequestParam String password,@RequestParam String name,@RequestParam String email){
         try { 
             usuarioService.valName(name);
-            Usuario user = new Usuario(name, password,email);
+            Usuario user = new Usuario(name,password,email);
             usuarioService.save(user);
             return ResponseEntity.ok(user);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
+    public ResponseEntity<Usuario> Login(@RequestParam String password,@RequestParam String email){
+        
+        Optional<Usuario> user=usuarioService.findByEmail(email);
+        
+        //  Comprobar si el user existe
+        if(user.isEmpty()){return ResponseEntity.status(HttpStatus.NOT_FOUND).build();}
+
+        // Comprobar si la contraseña es correcta
+        if(user.get().getPassword().equals(password)){return ResponseEntity.ok(user.get());/*login ok*/
+        }else{return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();/*Fail login*/}
     }
     public UsuarioController(UsuarioService usuarioService){
         this.usuarioService=usuarioService;
